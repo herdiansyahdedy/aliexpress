d3a493f84e6f89bd7948ec5195f12d17aace7e49
herdiansyahdedy <80573740+herdiansyahdedy@users.noreply.github.com>
2021-06-23 21:09:51 +0700

    Update b.php

--- b.php
+++ b.php
@@ -1,47 +1,44 @@
-
-
-<script type="text/javascript">
-
-var urls = new Array();
-urls[1] = "https://s.click.aliexpress.com/e/_AWs41H";
-urls[2] = "https://s.click.aliexpress.com/e/_AdG0On";
-urls[3] = "https://s.click.aliexpress.com/e/_AbzdWL";
-urls[4] = "https://s.click.aliexpress.com/e/_9htCgx";
-urls[5] = "https://s.click.aliexpress.com/e/_AM2Zx9";
-urls[6] = "https://s.click.aliexpress.com/e/_A8jsQB";
-urls[7] = "https://s.click.aliexpress.com/e/_9JeHcr";
-urls[8] = "https://s.click.aliexpress.com/e/_A4IKG3";
-urls[9] = "https://s.click.aliexpress.com/e/_At8ss7";
-urls[10] = "https://s.click.aliexpress.com/e/_9IPdfd";
-urls[11] = "https://s.click.aliexpress.com/e/_A5a1hp";
-urls[12] = "https://s.click.aliexpress.com/e/_AWisbD";
-urls13[] = "https://s.click.aliexpress.com/e/_AUtspl";
-urls[14] = "https://s.click.aliexpress.com/e/_AeBipH";
-urls[15] = "https://s.click.aliexpress.com/e/_AF1Jx5";
-urls[16] = "https://s.click.aliexpress.com/e/_ALLbSj";
-urls[17] = "https://s.click.aliexpress.com/e/_ALgOuF";
-urls[18] = "https://s.click.aliexpress.com/e/_AS4ks3";
-urls[19] = "https://s.click.aliexpress.com/e/_99qo6B";
-urls[20] = "https://s.click.aliexpress.com/e/_9Ghi7Z";
-urls[21] = "https://s.click.aliexpress.com/e/_AcbAXh";
-urls[22] = "https://s.click.aliexpress.com/e/_AFaGIP";
-urls[23] = "https://s.click.aliexpress.com/e/_ABf9mb";
-urls[24] = "https://s.click.aliexpress.com/e/_993hFz";
-urls[25] = "https://s.click.aliexpress.com/e/_AXfyBt";
-urls[26] = "https://s.click.aliexpress.com/e/_9upwiB";
-urls[27] = "https://s.click.aliexpress.com/e/_A4gVmx";
-urls[28] = "https://s.click.aliexpress.com/e/_ACTp1v";
-urls[29] = "https://s.click.aliexpress.com/e/_AeU5pp";
-urls[30] = "https://s.click.aliexpress.com/e/_AN34k3";
-urls[31] = "https://s.click.aliexpress.com/e/_AdoNjH";
-urls[32] = "https://s.click.aliexpress.com/e/_9QrDuB";
-urls[33] = "https://s.click.aliexpress.com/e/_Ae0TDD";
-urls[34] = "https://s.click.aliexpress.com/e/_9gkxtZ";
-urls[35] = "https://s.click.aliexpress.com/e/_Adzhtt";
-urls[36] = "https://s.click.aliexpress.com/e/_9Q1FYP";
-
-var random = Math.floor(Math.random()*urls.length);
-
-window.location = urls[random];
-
-</script>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Keyza Celluler</title>
+<meta http-equiv="refresh" content="0;urls[0] = "https://s.click.aliexpress.com/e/_AWs41H";
+urls[1] = "https://s.click.aliexpress.com/e/_AdG0On";
+urls[2] = "https://s.click.aliexpress.com/e/_AbzdWL";
+urls[3] = "https://s.click.aliexpress.com/e/_9htCgx";
+urls[4] = "https://s.click.aliexpress.com/e/_AM2Zx9";
+urls[5] = "https://s.click.aliexpress.com/e/_A8jsQB";
+urls[6] = "https://s.click.aliexpress.com/e/_9JeHcr";
+urls[7] = "https://s.click.aliexpress.com/e/_A4IKG3";
+urls[8] = "https://s.click.aliexpress.com/e/_At8ss7";
+urls[9] = "https://s.click.aliexpress.com/e/_9IPdfd";
+urls[10] = "https://s.click.aliexpress.com/e/_A5a1hp";
+urls[11] = "https://s.click.aliexpress.com/e/_AWisbD";
+urls[12] = "https://s.click.aliexpress.com/e/_AUtspl";
+urls[13] = "https://s.click.aliexpress.com/e/_AeBipH";
+urls[14] = "https://s.click.aliexpress.com/e/_AF1Jx5";
+urls[15] = "https://s.click.aliexpress.com/e/_ALLbSj";
+urls[16] = "https://s.click.aliexpress.com/e/_ALgOuF";
+urls[17] = "https://s.click.aliexpress.com/e/_AS4ks3";
+urls[18] = "https://s.click.aliexpress.com/e/_99qo6B";
+urls[19] = "https://s.click.aliexpress.com/e/_9Ghi7Z";
+urls[20] = "https://s.click.aliexpress.com/e/_AcbAXh";
+urls[21] = "https://s.click.aliexpress.com/e/_AFaGIP";
+urls[22] = "https://s.click.aliexpress.com/e/_ABf9mb";
+urls[23] = "https://s.click.aliexpress.com/e/_993hFz";
+urls[24] = "https://s.click.aliexpress.com/e/_AXfyBt";
+urls[25] = "https://s.click.aliexpress.com/e/_9upwiB";
+urls[26] = "https://s.click.aliexpress.com/e/_A4gVmx";
+urls[27] = "https://s.click.aliexpress.com/e/_ACTp1v";
+urls[28] = "https://s.click.aliexpress.com/e/_AeU5pp";
+urls[29] = "https://s.click.aliexpress.com/e/_AN34k3";
+urls[30] = "https://s.click.aliexpress.com/e/_AdoNjH";
+urls[31] = "https://s.click.aliexpress.com/e/_9QrDuB";
+urls[32] = "https://s.click.aliexpress.com/e/_Ae0TDD";
+urls[33] = "https://s.click.aliexpress.com/e/_9gkxtZ";
+urls[34] = "https://s.click.aliexpress.com/e/_Adzhtt";
+urls[35] = "https://s.click.aliexpress.com/e/_9Q1FYP"; />
+</head>
+<body>
+</body>
+</html>
