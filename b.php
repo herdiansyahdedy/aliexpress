91175e7eeb5155cb23ce1c4b21ed35fe8a12d12e
herdiansyahdedy <80573740+herdiansyahdedy@users.noreply.github.com>
2021-06-24 07:26:54 +0700

    Update b.php

--- b.php
+++ b.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Best Shopping Day</title>
-<meta http-equiv="refresh" content="5;URL='https://s.click.aliexpress.com/e/_AdG0On'" />
+<title>Store Smartphone Terpercaya</title>
+<meta http-equiv="refresh" content="0;URL='https://s.click.aliexpress.com/e/_AdG0On'" />
 </head>
 <body>
 </body>
