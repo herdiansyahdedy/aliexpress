ddbea2aa6f9db7dd7ae4ce32857527e34d1e7cb7
herdiansyahdedy <80573740+herdiansyahdedy@users.noreply.github.com>
2021-06-25 23:19:28 +0700

    Update b.php

--- b.php
+++ b.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 <title>Best Shopping Day</title>
-<meta http-equiv="refresh" content="0;URL='https://s.click.aliexpress.com/e/_9ijOgL'" />
+<meta http-equiv="refresh" content="0;URL='https://s.click.aliexpress.com/e/_AUrtIP'" />
 </head>
 <body>
 </body>
