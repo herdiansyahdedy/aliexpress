a5171977f504a5d2460ff89d27e2cd2466e06335
herdiansyahdedy <80573740+herdiansyahdedy@users.noreply.github.com>
2021-09-06 15:48:55 +0700

    Update b.php

--- b.php
+++ b.php
@@ -2,7 +2,8 @@
 <html>
 <head>
 <title>Peluang Bisnis Di Internert</title>
-<meta http-equiv="refresh" content="0;URL='https://ref.sharecash.co/dedy27'" />
+<meta http-equiv="refresh" content="0;URL='https://s.click.aliexpress.com/e/_AWjm70
+'" />
 </head>
 <body>
 </body>
